08c97d4185c95a167b10d6a52c652bd1f8a64a8b
Rufat <rufatxasanov1998@gmail.com>
2023-06-12 15:40:48 +0500

    ok

--- 37 Pokedex/script.js
+++ 37 Pokedex/script.js
@@ -36,17 +36,17 @@ const createPokemonCard = (pokemon) => {
   const pokemonEl = document.createElement('div')
   pokemonEl.classList.add('pokemon')
 
-  const name = pokemon.name[0].toUppercase() + pokemon.name.slice(1)
+  const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
 
   const id = pokemon.id.toString().padStart(3, '0')
   const poke_types = pokemon.types.map(type => type.type.name)
-  const type = main_types.find(type = poke_types.indexOf(type) > -1)
+  const type = main_types.find(type => poke_types.indexOf(type) > -1)
   const color = colors[type]
   pokemonEl.style.backgroundColor = color
 
   const pokemonInnerHMTL = `
   <div class="img-container">
-    <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="">
+    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png" alt="">
     </div>
   <div class="info">
     <span class="number">#${id}</span>
